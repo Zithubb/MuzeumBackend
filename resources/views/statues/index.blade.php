6b82bf6c857b588ac23b52bf817b7c82ceaa1b6d
Zita <ambrus.zita99@gmail.com>
2022-02-02 11:59:48 +0100

    ...

--- resources/views/statues/index.blade.php
+++ resources/views/statues/index.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
+
+
+@section('content')
+
     <p><a href="{{ route('home') }}">Vissza a főoldalra</a></p>
 
     <table>
@@ -23,14 +19,10 @@
             <td>{{ $statue->height }}</td>
             <td>{{ $statue->price }}</td>
             <td>
-                <form method="POST" action="{{ route('statues.destroy', $statue->id) }}">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Törlés</button>
-                </form>
+                @include('delete-statue-button', ['statue'=>$statue])
             </td>
         </tr>
     @endforeach
     </table>
-</body>
-</html>
+        <a href="{{ route('statues.create') }}">Create</a>
+@endsection
--- resources/views/statues/show.blade.php
+++ resources/views/statues/show.blade.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $statue->person }}</title>
-</head>
-<body>
+@extends('layout')
+
+@section('title', $statue->person)
+
+@section('content')
+        <a href="{{ route('statues.index') }}">Go to statue listing</a>
     <h1>The statue of {{ $statue->person }}</h1>
     <p>Height: {{ $statue->height }} cm</p>
     <p>Selling price: {{ $statue->price }} Ft</p>
-</body>
-</html>
+    <p> @include('delete-statue-button', ['statue'=>$statue])</p>
+        <a href="{{ route('statues.edit', $statue->id) }}">Edit</a>
+@endsection
